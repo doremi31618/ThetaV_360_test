3c98f91c5f8e53d26eaf1b9ab03ebb441d23a640
Ju Chun Ko <dablog@gmail.com>
2019-05-27 18:25:27 +0800

    upload steam vr

--- Assets/Scripts/ThetaV/WebCamTest.cs
+++ Assets/Scripts/ThetaV/WebCamTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WebCamTest : MonoBehaviour
 {
-    public int numberOfCamera = 0;
+   // public int numberOfCamera = 0;
     // Use this for initialization
     void Start()
     {
@@ -15,11 +15,22 @@ public class WebCamTest : MonoBehaviour
         for (int i = 0; i < devices.Length; i++)
         {
             Debug.Log(devices[i].name);
+            if(devices[i].name == "ManyCam Virtual Webcam")
+            {
+                string camName = devices[i].name;
+                Debug.Log("The WebCam name is" + camName);
+                myCam.deviceName = camName;
+                renderer.material.mainTexture = myCam;
+                myCam.Play();
+                break;
+            }
         }
+        /*
         string camName = devices[numberOfCamera].name;
         Debug.Log("The WebCam name is" + camName);
         myCam.deviceName = camName;
         renderer.material.mainTexture = myCam;
         myCam.Play();
+        */
     }
 }
